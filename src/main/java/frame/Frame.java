3ebe7fe376c5fe0ec75745a53838ad284396144f
George Mao <zhongqilin@gmail.com>
2022-12-05 01:45:08 -0500

    add the Frame class

---
+++ src/main/java/frame/Frame.java
@@ -0,0 +1,13 @@
+package frame;
+
+/**
+ * The class for containing all the information to display a frame in the application.
+ */
+public class Frame {
+
+    /**
+     * The name of the fxml file that generates the structure of the frame.
+     */
+    String fxmlName;
+
+}
