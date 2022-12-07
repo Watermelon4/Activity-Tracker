95dbe939fe06ecbbc2071312bb18011cf2e29c4a
George Mao <zhongqilin@gmail.com>
2022-12-07 03:32:07 -0500

    remove observer package and test stylesheets

--- src/main/java/observer/ISubscriber.java
+++
@@ -1,9 +0,0 @@
-package observer;
-
-public interface ISubscriber {
-
-    /**
-     * Receives new styles from the StyleManager and applies it to the frame.
-     */
-    void update(String style, String styleSheet);
-}
